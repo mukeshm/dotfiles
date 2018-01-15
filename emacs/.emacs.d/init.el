69c3ff05fcf6671d758ffa6275d394b68c792c80
Mukesh M <me@mukeshm.com>
2018-01-15 19:50:26 +0530

    Save backup files to backup directory

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,51 +1,33 @@
 ;; init.el --- Emacs configuration
 
-;; INSTALL PACKAGES
-;; --------------------------------------
-
-(require 'package)
-
-;; Define package repositories
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
-
-;; Load and activate emacs packages. Do this first so that the
-;; packages are loaded before you start trying to modify them.
-;; This also sets the load path.
-(package-initialize)
-
-;; Download the ELPA archive description if needed.
-;; This informs Emacs about the latest versions of all packages, and
-;; makes them available for download.
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; The packages you want installed. You can also install these
-;; manually with M-x package-install
-;; Add in your own as you wish:
-(defvar myPackages
-  '())
-
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-      myPackages)
-
 ;; BASIC CUSTOMIZATION
 ;; --------------------------------------
 
 ;; hide the startup message
 (setq inhibit-startup-message t)
-;; enable line numbers globally
-(global-linum-mode 1)
-;; disable the menu bar
+
+;; disable the menu-bar, tool-bar and scroll-bar 
 (menu-bar-mode -1)
-;; disable the toolbar
 (tool-bar-mode -1)
-;; disable the scrollbar
 (scroll-bar-mode -1)
+
 ;; highlight matching pairs of parentheses and other characters
 (show-paren-mode 1)
+
+;; enable line numbers globally
+(global-linum-mode 1)
+
 ;; insert matching delimiters
 (electric-pair-mode 1)
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
+
 ;; init.el ends here 
+
